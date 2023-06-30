a6b2b12d434fca14b46f134ec1ff456e02269b96
CryDeveloper <fordefdevat@gmail.com>
2023-07-01 02:58:15 +0300

    доредачила авторизацию, но таймер так и не вспомнила

--- Pages/Auth.xaml.cs
+++ Pages/Auth.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using TNS__provider_.Classes;
 
 namespace TNS__provider_.Pages
@@ -22,13 +24,30 @@ namespace TNS__provider_.Pages
     /// </summary>
     public partial class Auth : Page
     {
+        private DispatcherTimer dispatcher;
+        //dispatcher = new DispatcherTimer();
+        //dispatcher.Interval = new TimeSpan(0, 0, 1);
+        //dispatcher.Tick += new EventHandler(TimerEnd);
+        //dispatcher.Start();
         private string chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM[];',./{}:|<>?1234567890!@#$%^&*()_+=-";
         Random random = new Random();
         private string code;
         public Auth()
         {
             InitializeComponent();
+
         }
+
+        /// <summary>
+        /// Метод таймера, возврающий сообщение о том, что время вышло.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerEnd(object sender, EventArgs e)
+        {
+            
+        }
+
         /// <summary>
         /// Очистка всех полей на форме авторизации
         /// </summary>
@@ -38,7 +57,10 @@ namespace TNS__provider_.Pages
         {
             Number.Clear();
             Password.Clear();
+            Password.IsEnabled = false;
             Code.Clear();
+            ForCodePanel.IsEnabled = false;
+            Entry.IsEnabled = false;
         }
         /// <summary>
         /// Идентификация пользователя при вводе номера.
@@ -93,8 +115,8 @@ namespace TNS__provider_.Pages
         private void GenerateCode()
         {
             Code.Clear();
-            code = "1"/*new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray())*/;
-            MessageBox.Show($"Запомните следующий код регистрации: {code}");
+            code =/* "1"*/new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+            MessageBox.Show($"Запомните следующий код авторизации: {code}");
             /*я забыла код для таймера*/
         }
 
@@ -142,11 +164,9 @@ namespace TNS__provider_.Pages
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void UpdateCode_Click(object sender, RoutedEventArgs e)
+        private void imgUpdate_MouseDown(object sender, MouseButtonEventArgs e)
         {
             GenerateCode();
         }
-
-
     }
 }
